3ae0af64af40ff24ed2af3a1f6e08fc2f208a1c3
Besitzer-R3094094 <Besitzer-R3094094@NB-R3094094.fritz.box>
2016-11-06 10:28:47 +0100

    testing

---
+++ src/test1/Test.java
@@ -0,0 +1,24 @@
+package test1;
+
+public class Test {
+
+	public Test() {
+	}
+	
+	private String sanitize( String str ) {
+		if( !str.isEmpty() ) {
+		str = str.trim();
+		}
+		str = str.toLowerCase();
+		return str;
+		}
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		String s = "Frank";
+		System.out.println(s);
+test.sanitize(s);
+System.out.println(s);
+	}
+//neu
+}
